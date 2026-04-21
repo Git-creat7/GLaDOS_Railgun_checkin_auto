fecb62a9749fb01b826507c5bc6b28155c92486e
NeoKzcod <3516988334@qq.com>
2026-04-21 13:28:07 +0800

    Update railgun.py

--- railgun.py
+++ railgun.py
@@ -22,7 +22,7 @@ def start():
     referer = "https://railgun.info/console/checkin"
     origin = "https://railgun.info"
     useragent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36"
-    payload = {"token": "glados.one"}
+    payload = {"token": "railgun.one"}
 
     push_title = "Railgun签到结果"
 
